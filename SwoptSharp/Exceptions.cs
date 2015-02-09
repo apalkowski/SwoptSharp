4fa290ed7b648305dc66b02fa2fdf510607f63a2
Aleksander Pałkowski <aleksander.palkowski@gmail.com>
2015-02-09 12:30:57 +0100

    Added NodeAlreadyExistsException exception

--- SwoptSharp/Exceptions.cs
+++ SwoptSharp/Exceptions.cs
@@ -3,6 +3,49 @@
 namespace SwoptSharp
 {
     [Serializable]
+    public class NodeAlreadyExistsException : Exception
+    {
+        #region Private Fields
+
+        private const string defaultMessage = "There is already a node with the specified key value.";
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public NodeAlreadyExistsException()
+            : base(defaultMessage)
+        {
+        }
+
+        public NodeAlreadyExistsException(Exception innerException)
+            : base(defaultMessage, innerException)
+        {
+        }
+
+        public NodeAlreadyExistsException(string message)
+            : base(message)
+        {
+        }
+
+        public NodeAlreadyExistsException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region Protected Constructors
+
+        protected NodeAlreadyExistsException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        #endregion Protected Constructors
+    }
+
+    [Serializable]
     public class NodeNotFoundException : Exception
     {
         #region Private Fields
